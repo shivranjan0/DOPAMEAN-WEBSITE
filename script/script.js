b9b8cf71864627757d11793ec1a24d049c21310c
shivranjan <shivranjankumar917@gmail.com>
2025-09-19 17:56:23 +0530

    done small changes

--- script/script.js
+++ script/script.js
@@ -16,3 +16,30 @@ setInterval(updateClock, 1000);
 
 // Run it once immediately on load
 updateClock();
+
+
+
+
+// side nav clock
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Function to update time in both sidebar & footer
+    function updateClock() {
+        let now = new Date();
+
+
+        let hours = now.getHours().toString().padStart(2, '0');
+        let minutes = now.getMinutes().toString().padStart(2, '0');
+        let seconds = now.getSeconds().toString().padStart(2, '0');
+        let timeString = `[ INDIA ${hours}:${minutes}:${seconds} HH ]`;
+        let sidebarTime = document.getElementById("time1");
+        let footerTime = document.getElementById("bott3");
+
+        if (sidebarTime) sidebarTime.textContent = timeString;
+        if (footerTime) footerTime.textContent = timeString;
+    }
+
+    setInterval(updateClock, 1000);
+    updateClock();
+});
